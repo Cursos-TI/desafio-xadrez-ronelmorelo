4db17e65029c55672025290f4c73f1fec7f6250d
ronelmorelo <ronelmorelo@gmail.com>
2025-06-06 13:05:06 +0000

    Adicionando codigo de nivel aventureiro (movimento do cavalo)

--- xadrez.c
+++ xadrez.c
@@ -10,6 +10,9 @@ int main() {
     const int movimentoBispo = 5;
     const int movimentoTorre = 5;
     const int movimentoRainha = 8;
+    
+    const int movimentoCavaloVertical = 2;
+    const int movimentoCavaloHorizontal = 1;
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
@@ -39,6 +42,16 @@ int main() {
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
+    printf("\n---Cavalo---\n");
+    contadorWhile = 0; // Reutilizando contador para while
+    for(int i = 0; i < movimentoCavaloHorizontal; i++){
+        while (contadorWhile < movimentoCavaloVertical)
+        {
+            printf("Baixo, ");
+            contadorWhile++;
+        }
+        printf("Esquerda\n");
+    }
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
